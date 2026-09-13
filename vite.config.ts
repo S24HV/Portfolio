dea22b15d3447cdcf8127b5a43c13b94dd65b0cf
NEXIT <amirsuhov@gmail.com>
2026-09-13 19:53:26 +0500

    fix: add base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // или другой ваш плагин, если используется Vue/Preact
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio",
-  server: {
-    port: 3000,
-  },
-});
+  base: '/Portfolio/', // <--- ОБЯЗАТЕЛЬНО ДОБАВЬТЕ ЭТУ СТРОКУ
+})
