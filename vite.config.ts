dc5619302ed3939e03d2219e8c2640cda6ee59ab
NEXIT <amirsuhov@gmail.com>
2026-09-13 20:21:28 +0500

    Updates

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // или другой ваш плагин, если используется Vue/Preact
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio/', // <--- ОБЯЗАТЕЛЬНО ДОБАВЬТЕ ЭТУ СТРОКУ
-})
